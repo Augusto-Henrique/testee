b2078c486dc8ea1f63ecab02a9dfd37bf962552f
João Victor Duarte <joaodicu387@gmail.com>
2021-07-09 20:49:16 -0300

    Codificando minhas personalidades de desenvolvedor

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+const desenvolvedor = {
+    nome: "João Victor",
+    idade: 20,
+    funcao: "Desenvolver código JavaScript"
+}
+
+console.log(`Meu nome é ${desenvolvedor.nome}, tenho ${desenvolvedor.idade} e a minha função é ${desenvolvedor.funcao}`);
