582be6becca4f7aeab02de05992daae1a3547968
João Victor Duarte <joaodicu387@gmail.com>
2021-07-09 21:23:52 -0300

    Ola mundo

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ const desenvolvedor = {
     funcao: "Desenvolver código JavaScript"
 }
 
+console.log("Olá, mundo");
 console.log(`Meu nome é ${desenvolvedor.nome}, tenho ${desenvolvedor.idade} e a minha função é ${desenvolvedor.funcao}`);
