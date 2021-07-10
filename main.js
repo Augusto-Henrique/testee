823ece8cf496aba3318b68f1563de7fa98cd10a2
João Victor Duarte <joaodicu387@gmail.com>
2021-07-09 21:26:52 -0300

    Deletando Ola mundo

--- main.js
+++ main.js
@@ -4,5 +4,4 @@ const desenvolvedor = {
     funcao: "Desenvolver código JavaScript"
 }
 
-console.log("Olá, mundo");
 console.log(`Meu nome é ${desenvolvedor.nome}, tenho ${desenvolvedor.idade} e a minha função é ${desenvolvedor.funcao}`);
